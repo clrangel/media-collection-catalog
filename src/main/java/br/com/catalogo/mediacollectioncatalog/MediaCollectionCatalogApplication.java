3e96f6e6545d99157d18d2397ab809a641e6337c
Camilo LR <autumndrums@hotmail.com>
2026-03-19 18:56:04 -0300

    chore: configuração inicial do projeto (media-collection-catalog)

---
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/MediaCollectionCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.catalogo.mediacollectioncatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaCollectionCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaCollectionCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/catalogo/mediacollectioncatalog/MediaCollectionCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catalogo.mediacollectioncatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaCollectionCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
