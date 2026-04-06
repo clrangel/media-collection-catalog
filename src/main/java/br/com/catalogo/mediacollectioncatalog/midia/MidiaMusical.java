3f3e9d54309568c6081cd32c312c71cef9d00e52
Camilo LR <autumndrums@hotmail.com>
2026-04-06 15:19:52 -0300

    feat: criar entidade abstrata MidiaMusical com herança e relacionamentos
    - adiciona classe MidiaMusical estendendo Midia
    - define enums genero e categoriaDisco
    - implementa relacionamento ManyToOne com Artista
    - implementa relacionamento OneToMany com Faixa
    - configura cascade e orphanRemoval

---
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/midia/MidiaMusical.java
@@ -0,0 +1,35 @@
+package br.com.catalogo.mediacollectioncatalog.midia;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "midias_musicais")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public abstract class MidiaMusical extends Midia{
+
+    @Enumerated(EnumType.STRING)
+    private GeneroMusical genero;
+
+    @Enumerated(EnumType.STRING)
+    private CategoriaDisco categoria;
+
+    @Size(max = 250)
+    @Column(nullable = false, length = 250)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "artista_id", nullable = false)
+    private Artista artista;
+
+    @OneToMany(mappedBy = "midiaMusical", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Faixa> faixas = new ArrayList<>();
+}
