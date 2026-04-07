cbed96275c1639c51650028f6219ee1bdeb1a21d
Camilo LR <autumndrums@hotmail.com>
2026-04-07 16:20:20 -0300

    feat: adiciona enum TipoCD para representar tipos de embalagem de CDs

---
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/midia/musical/enums/TipoCD.java
@@ -0,0 +1,16 @@
+package br.com.catalogo.mediacollectioncatalog.midia.musical.enums;
+
+public enum TipoCD {
+
+    JEWEL_CASE,
+    FATBOX,
+    DIGIPAK,
+    DIGISLEEVE,
+    DIGISTAK,
+    SLIPCASE,
+    CARDBOARD_SLEEVE,
+    BOX_SET,
+    STEELBOOK,
+    MINI_LP,
+    OUTRO;
+}
