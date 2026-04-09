59abb2902e2034f4a8f64f27e8ba8c192f177b29
Camilo LR <autumndrums@hotmail.com>
2026-04-09 17:11:06 -0300

    feat: adiciona entidade DVD como especialização de MidiaVideo

---
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/midia/video/domain/DVD.java
@@ -0,0 +1,23 @@
+package br.com.catalogo.mediacollectioncatalog.midia.video.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name = "dvds")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class DVD extends MidiaVideo{
+
+    @NotNull
+    @Column(nullable = false)
+    private Integer numeroDiscos;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoEdicao tipoEdicao;
+}
