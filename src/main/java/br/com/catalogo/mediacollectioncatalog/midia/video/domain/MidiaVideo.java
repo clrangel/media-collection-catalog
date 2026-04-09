a34251cf934fdae014a74ce8b0efb8e6eff0f702
Camilo LR <autumndrums@hotmail.com>
2026-04-09 16:10:30 -0300

    feat(video): adiciona enum GeneroFilme com os principais gêneros do domínio de mídias de vídeo

--- src/main/java/br/com/catalogo/mediacollectioncatalog/midia/video/domain/MidiaVideo.java
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/midia/video/domain/MidiaVideo.java
@@ -1,5 +1,6 @@
 package br.com.catalogo.mediacollectioncatalog.midia.video.domain;
 
+import br.com.catalogo.mediacollectioncatalog.diretor.domain.Diretor;
 import br.com.catalogo.mediacollectioncatalog.midia.Midia;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
---
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/midia/video/domain/enums/GeneroFilme.java
@@ -0,0 +1,25 @@
+package br.com.catalogo.mediacollectioncatalog.midia.video.domain.enums;
+
+public enum GeneroFilme {
+
+        ACAO,
+        AVENTURA,
+        COMEDIA,
+        DRAMA,
+        ROMANCE,
+        TERROR,
+        SUSPENSE,
+        FICCAO_CIENTIFICA,
+        FANTASIA,
+        ANIMACAO,
+        DOCUMENTARIO,
+        BIOGRAFIA,
+        GUERRA,
+        POLICIAL,
+        MISTERIO,
+        MUSICAL,
+        ESPORTE,
+        FAROESTE,
+        HISTORICO,
+        CRIME;
+}
