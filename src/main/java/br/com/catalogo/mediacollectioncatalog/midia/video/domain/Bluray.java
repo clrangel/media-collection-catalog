80d25ddae070d630a76a5ad9038030c2b5ac4006
Camilo LR <autumndrums@hotmail.com>
2026-04-09 17:20:00 -0300

    feat: adiciona entidade Bluray com suporte a tipo de edição e resolução

---
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/midia/video/domain/Bluray.java
@@ -0,0 +1,27 @@
+package br.com.catalogo.mediacollectioncatalog.midia.video.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name = "blurays")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Bluray extends MidiaVideo{
+
+    @NotNull
+    @Column(nullable = false)
+    private Integer numeroDiscos;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoEdicao tipoEdicao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Resolucao resolucao;
+}
