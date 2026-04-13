dc47975de3a66dc7b7ad411e3ae954f902332695
Camilo LR <autumndrums@hotmail.com>
2026-04-13 17:50:50 -0300

    feat: adiciona controller e endpoint de cadastro de artista

---
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/artista/controller/ArtistaController.java
@@ -0,0 +1,27 @@
+package br.com.catalogo.mediacollectioncatalog.artista.controller;
+
+import br.com.catalogo.mediacollectioncatalog.artista.dtos.ArtistaRequestDTO;
+import br.com.catalogo.mediacollectioncatalog.artista.dtos.ArtistaResponseDTO;
+import br.com.catalogo.mediacollectioncatalog.artista.service.ArtistaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/artistas")
+public class ArtistaController {
+
+    private final ArtistaService service;
+
+    @PostMapping
+    public ResponseEntity<ArtistaResponseDTO> cadastrarArtista(@RequestBody ArtistaRequestDTO dto){
+        ArtistaResponseDTO artistaCriado = service.cadastrarArtista(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(artistaCriado);
+    }
+}
