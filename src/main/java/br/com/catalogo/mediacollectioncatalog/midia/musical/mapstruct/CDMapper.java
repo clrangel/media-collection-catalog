6e64bd2ef3b83691ce0e420ec4abdc422ed20b6a
Camilo LR <autumndrums@hotmail.com>
2026-04-14 18:03:48 -0300

    feat: implementa CDMapper com conversão de DTO para entidade usando MapStruct

---
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/midia/musical/mapstruct/CDMapper.java
@@ -0,0 +1,26 @@
+package br.com.catalogo.mediacollectioncatalog.midia.musical.mapstruct;
+
+import br.com.catalogo.mediacollectioncatalog.midia.musical.domain.CD;
+import br.com.catalogo.mediacollectioncatalog.midia.musical.dto.cddto.CDRequestDTO;
+import br.com.catalogo.mediacollectioncatalog.midia.musical.dto.cddto.CDResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CDMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "artista", ignore = true)
+    @Mapping(target = "genero", expression = "java(GeneroMusical.valueOf(dto.genero()))")
+    @Mapping(target = "categoria", expression = "java(CategoriaDisco.valueOf(dto.categoria()))")
+    @Mapping(target = "formatoDisco", expression = "java(FormatoDisco.valueOf(dto.formatoDisco()))")
+    @Mapping(target = "tipoCD", expression = "java(TipoCD.valueOf(dto.tipoCD()))")
+    CD toEntity(CDRequestDTO dto);
+
+    @Mapping(target = "artistaId", source = "artista.id")
+    @Mapping(target = "genero", expression = "java(cd.getGenero().name())")
+    @Mapping(target = "categoria", expression = "java(cd.getCategoria().name())")
+    @Mapping(target = "formatoDisco", expression = "java(cd.getFormatoDisco().name())")
+    @Mapping(target = "tipoCD", expression = "java(cd.getTipoCD().name())")
+    CDResponseDTO toDTO(CD cd);
+}
