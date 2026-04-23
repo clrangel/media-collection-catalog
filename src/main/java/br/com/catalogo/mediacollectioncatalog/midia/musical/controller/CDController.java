4fca6bf8be80c17ac1286db0aeb9643e29efb309
Camilo LR <autumndrums@hotmail.com>
2026-04-23 15:59:15 -0300

    docs: melhora documentação da API adicionando @Operation nos endpoints no CDController

--- src/main/java/br/com/catalogo/mediacollectioncatalog/midia/musical/controller/CDController.java
+++ src/main/java/br/com/catalogo/mediacollectioncatalog/midia/musical/controller/CDController.java
@@ -3,6 +3,7 @@ package br.com.catalogo.mediacollectioncatalog.midia.musical.controller;
 import br.com.catalogo.mediacollectioncatalog.midia.musical.dto.cddto.CDRequestDTO;
 import br.com.catalogo.mediacollectioncatalog.midia.musical.dto.cddto.CDResponseDTO;
 import br.com.catalogo.mediacollectioncatalog.midia.musical.service.CDService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,18 +19,32 @@ public class CDController {
 
     private final CDService service;
 
+    @Operation(
+            summary = "Cadastrar novo CD",
+            description = "Cria um novo CD no sistema com base nos dados informados"
+    )
     @PostMapping
     public ResponseEntity<CDResponseDTO> cadastrarCD(@Valid @RequestBody CDRequestDTO dto){
         CDResponseDTO cdCriado = service.cadastrarCD(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(cdCriado);
     }
 
+
+    @Operation(
+            summary = "Deletar CD",
+            description = "Remove um CD do sistema com base no ID informado"
+    )
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deletarCD(@PathVariable Long id){
         service.deletarCD(id);
         return ResponseEntity.noContent().build();
     }
 
+
+    @Operation(
+            summary = "Atualizar CD",
+            description = "Atualiza os dados de um CD existente no sistema com base no ID informado"
+    )
     @PutMapping("/{id}")
     public ResponseEntity<CDResponseDTO> atualizarCD(
             @PathVariable Long id,
@@ -39,12 +54,22 @@ public class CDController {
         return ResponseEntity.ok(atualizado);
     }
 
+
+    @Operation(
+            summary = "Buscar CD por ID",
+            description = "Retorna os dados de um CD específico com base no ID informado"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<CDResponseDTO> buscarCDPorId(@PathVariable Long id) {
         CDResponseDTO cd = service.buscarCDPorId(id);
         return ResponseEntity.ok(cd);
     }
 
+
+    @Operation(
+            summary = "Listar todos os CDs",
+            description = "Retorna uma lista com todos os CDs cadastrados no sistema"
+    )
     @GetMapping
     public ResponseEntity<List<CDResponseDTO>> listarTodosCDs() {
         List<CDResponseDTO> cds = service.listarTodosCDs();
