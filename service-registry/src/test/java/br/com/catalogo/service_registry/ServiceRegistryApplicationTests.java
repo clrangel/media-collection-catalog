b9ddd328781d40bb3d0350494ad7faba27c442b5
Camilo LR <autumndrums@hotmail.com>
2026-08-03 18:22:36 -0300

    chore(service-registry): cria projeto inicial Service Registry e ajusta pom.xml para compatibilidade com Spring Boot 3.5.12

---
+++ service-registry/src/test/java/br/com/catalogo/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catalogo.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
